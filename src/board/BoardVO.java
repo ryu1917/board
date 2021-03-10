6540f46857043402a5fdefc9127f12c822ada296
wooki <hek4174@daum.net>
2021-03-10 21:09:13 +0900

    VO

---
+++ src/board/BoardVO.java
@@ -0,0 +1,32 @@
+package board;
+
+public class BoardVO {
+	private int num;
+	private String title;
+	private String writer;
+	private String content;
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getWriter() {
+		return writer;
+	}
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+}
