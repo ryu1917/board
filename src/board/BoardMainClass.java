23c9e079e8048709e86decc55089f50e6b4f4e23
younsik.choi <nadays3019@gmail.com>
2021-03-10 21:32:01 +0900

    VO pull 및 3번 5번 내용 완료

--- src/board/BoardMainClass.java
+++ src/board/BoardMainClass.java
@@ -2,6 +2,8 @@ package board;
 
 import java.util.Scanner;
 
+import member.MemberVO;
+
 public class BoardMainClass {
 	public static void main(String[] args) {
 		Scanner input=new Scanner(System.in);
@@ -13,9 +15,35 @@ public class BoardMainClass {
 			switch(sel) {
 			case 1:break;
 			case 2:break;
-			case 3:break;
+			case 3:
+				System.out.print("탈퇴 할 회원 ID를 입력 : ");
+				String id = input.next();
+				int result = db.deleteMember(id);
+				if(result ==1) {
+					System.out.println("성공적으로 삭제했습니다.");
+				}else {
+					System.out.println("삭제 중 문제가 발생했습니다.");
+				}
+				break;
 			case 4:break;
-			case 5:break;
+			case 5:
+				System.out.println("수정 할 글 번호 입력 : ");
+				bo.setNum(input.nextInt());
+				System.out.println("수정 할 글 제목 입력 : ");
+				bo.setTitle(input.next());
+				System.out.println("수정 할 작가 명 입력 : ");
+				bo.setWriter(input.next());
+				System.out.println("수정 할 내용 입력 : ");
+				bo.setContent(input.next());
+				
+				int re = db.modifyBoard(bo);
+				if(re ==1) {
+					System.out.println("성공적으로 수정했습니다.");
+				}else {
+					System.out.println("수정 중 문제가 발생했습니다.");
+				}
+				
+				break;
 			case 6:break;
 			
 			}
