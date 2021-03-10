09c123b1e41a9d915d6f2c6947104bc55d67e514
Great-Root <icujoa@gmail.com>
2021-03-11 01:47:43 +0900

    Main UI 변경 및 Service interface 작성

--- src/board/BoardMainClass.java
+++ src/board/BoardMainClass.java
@@ -7,71 +7,121 @@ import member.MemberVO;
 
 
 public class BoardMainClass {
+	
 	public static void main(String[] args) {
+		//로그인 정보
+		MemberVO loginData = new MemberVO();
+		boolean isLogin = true;
+		
 		Scanner input=new Scanner(System.in);
-		DBClass db=new DBClass();
-		MemberVO m=new MemberVO();
-		BoardVO bo=new BoardVO();
+		IBoardService service = new BoardService();
 		while(true) {
-			int sel=input.nextInt();
-			switch(sel) {
-			case 1:
-				System.out.print("ID 입력 : ");
-				m.setId(input.next());
-				System.out.print("PW 입력 : ");
-				m.setPw(input.next());
-				System.out.print("이름 입력 : ");
-				m.setName(input.next());
-				System.out.print("나이 입력 : ");
-				m.setAge(input.nextInt());
-				db.insertMember(m);
-				break;
-			case 2:
-				db.getMember();
-				break;
-			case 3:
-				System.out.print("탈퇴 할 회원 ID를 입력 : ");
-				String id = input.next();
-				int result = db.deleteMember(id);
-				if(result ==1) {
-					System.out.println("성공적으로 삭제했습니다.");
-				}else {
-					System.out.println("삭제 중 문제가 발생했습니다.");
+			if(!isLogin) {
+				System.out.println("1.로그인 2.회원가입 3.프로그램 종료");
+				switch(input.nextInt()) {
+				case 1:
+					MemberVO login = new MemberVO();
+					System.out.print("아이디 : ");
+					login.setId(input.next());
+					System.out.print("비밀번호 : ");
+					login.setPw(input.next());
+					isLogin = service.login(login);
+					if(isLogin) {
+						loginData = service.getInfo(login);
+						System.out.println(loginData.getId()+" 님 환영합니다!");
+					}else {
+						System.out.println("아이디 또는 비밀번호가 다릅니다.");
+					}
+					break;
+				case 2:
+					MemberVO regi = new MemberVO();
+					System.out.print("ID 입력 : ");
+					String id = input.next();
+					//ID 중복 확인
+					if(!service.overlap(id)) {
+						regi.setId(id);
+						System.out.print("PW 입력 : ");
+						regi.setPw(input.next());
+						System.out.print("이름 입력 : ");
+						regi.setName(input.next());
+						System.out.print("나이 입력 : ");
+						regi.setAge(input.nextInt());
+						service.register(regi);
+						System.out.println(id +"님의 가입을 환영합니다!");
+					}else {
+						System.out.println("이미 존재하는 ID 입니다.");
+					}
+					break;
+				default:
+					System.out.println("프로그램 종료");
+					return;
 				}
-			case 4:
-				System.out.print("제목 : ");
-				bo.setTitle(input.next());
-				System.out.print("작성자 : ");
-				bo.setWriter(input.next());
-				System.out.print("내용 : ");
-				bo.setContent(input.next());
-				db.insertBoard(bo);
-				break;
-			case 5:
-				System.out.println("수정 할 글 번호 입력 : ");
-				bo.setNum(input.nextInt());
-				System.out.println("수정 할 글 제목 입력 : ");
-				bo.setTitle(input.next());
-				System.out.println("수정 할 작가 명 입력 : ");
-				bo.setWriter(input.next());
-				System.out.println("수정 할 내용 입력 : ");
-				bo.setContent(input.next());
-				
-				int re = db.modifyBoard(bo);
-				if(re ==1) {
-					System.out.println("성공적으로 수정했습니다.");
-				}else {
-					System.out.println("수정 중 문제가 발생했습니다.");
+			}else {
+				System.out.println("1.글목록 2.글작성 3.글수정 4.글삭제 5.로그아웃 6.회원탈퇴");
+				switch(input.nextInt()) {
+				case 1:
+					service.showList();
+					break;
+				case 2:
+					BoardVO post = new BoardVO();
+					System.out.print("제목 : ");
+					post.setTitle(input.next());
+					System.out.print("내용 : ");
+					post.setContent(input.next());
+					post.setWriter(loginData.getId());
+					service.write(post);
+					break;
+				case 3:
+					System.out.print("수정할 글 번호 : ");
+					BoardVO modifyInfo = service.show(input.nextInt());
+					//수정할 게시글 작성자와 로그인 정보가 일치할때
+					if(modifyInfo.getWriter().equals(loginData.getId())) {
+						System.out.print("기존 제목 >>> ");
+						System.out.println(modifyInfo.getTitle());
+						System.out.print("수정할 제목 : ");
+						modifyInfo.setTitle(input.next());
+						System.out.print("기존 내용 >>> ");
+						System.out.println(modifyInfo.getContent());
+						System.out.print("수정할 내용 : ");
+						modifyInfo.setContent(input.next());
+					}else {
+						System.out.print("글 작성자가 아닙니다.");
+					}
+					break;
+				case 4:
+					System.out.print("삭제할 글 번호 : ");
+					BoardVO deleteInfo = service.show(input.nextInt());
+					//삭제할 게시글 작성자와 로그인 정보가 일치할때
+					if(deleteInfo.getWriter().equals(loginData.getId())) {
+						System.out.println("삭제정보) 글번호 : "+deleteInfo.getNum()+"제목 : "+deleteInfo.getTitle());
+						System.out.println("정말로 삭제하시겠습니까? (Y/N)");
+						if(input.next().toUpperCase().equals("Y")) {
+							service.delete(deleteInfo);
+							System.out.println("게시글이 삭제되었습니다.");
+						}else {
+							System.out.println("게시글 삭제가 취소되었습니다.");
+						}
+					}else {
+						System.out.print("글 작성자가 아닙니다.");
+					}
+					break;
+				case 5:
+					System.out.println("로그아웃 되었습니다.");
+					isLogin = false;
+					break;
+				case 6:
+					System.out.println("정말로 회원 탈퇴를 하시겠습니까? (Y/N)");
+					if(input.next().toUpperCase().equals("Y")) {
+						System.out.println(loginData.getId()+"님 다음에 다시 만나요~");
+						service.byebye(loginData);
+						isLogin = false;
+					}else {
+						System.out.println("휴~");
+					}
+				default:
 				}
-				
-				break;
-			case 6:
-				System.out.println("삭제할 글 번호를 입력해주세요");
-				db.deleteBoard(input.nextInt());
-				break;
-			
 			}
-			
+				
 		}
 		
 	}
---
+++ src/board/BoardService.java
@@ -0,0 +1,62 @@
+package board;
+
+import member.MemberVO;
+
+public class BoardService implements IBoardService {
+
+	@Override
+	public boolean login(MemberVO m) {
+		return false;
+	}
+	
+	@Override
+	public MemberVO getInfo(MemberVO m) {
+		return null;
+	}
+
+	@Override
+	public void logout() {
+
+	}
+	
+	@Override
+	public boolean overlap(String id) {
+		return false;
+	}
+
+	@Override
+	public void register(MemberVO m) {
+
+	}
+
+	@Override
+	public void byebye(MemberVO m) {
+
+	}
+
+	@Override
+	public void write(BoardVO b) {
+
+	}
+
+	@Override
+	public BoardVO show(int num) {
+		return null;
+	}
+
+	@Override
+	public void modify(BoardVO b) {
+
+	}
+
+	@Override
+	public void delete(BoardVO b) {
+
+	}
+
+	@Override
+	public void showList() {
+
+	}
+
+}
