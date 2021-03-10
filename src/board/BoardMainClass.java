f31ba6c96d84530725f658fd61ef0c8a70e3a852
jongmin.ryu <ryu1917@gmail.com>
2021-03-10 20:14:32 +0900

    main틀 완성

---
+++ src/board/BoardMainClass.java
@@ -0,0 +1,26 @@
+package board;
+
+import java.util.Scanner;
+
+public class BoardMainClass {
+	public static void main(String[] args) {
+		Scanner input=new Scanner(System.in);
+		DBClass db=new DBClass();
+		MemberVO m=new MemberVO();
+		BoardVO bo=new BoardVO();
+		while(true) {
+			int sel=input.nextInt();
+			switch(sel) {
+			case 1:break;
+			case 2:break;
+			case 3:break;
+			case 4:break;
+			case 5:break;
+			case 6:break;
+			
+			}
+			
+		}
+		
+	}
+}
